e40322526a2051ec2d4ba3dd8213c2b409f8b4a2
ShiftyShifter <58483864+ShiftyShifter@users.noreply.github.com>
2022-12-22 00:51:55 +0300

    initial commit

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -5,24 +5,20 @@ const configuration = new Configuration({
 });
 const openai = new OpenAIApi(configuration);
 
+const imageURL = "https://oaidalleapiprodscus.blob.core.windows.net/private/org-Pg16Ipw0R27FHcmd8aIVP2CL/user-gIsN0kHGTba9DD7b4h7369ZR/img-8kEz71FAJxcSJOrQ6yyxyG9G.png?st=2022-12-20T18%3A09%3A20Z&se=2022-12-20T20%3A09%3A20Z&sp=r&sv=2021-08-06&sr=b&rscd=inline&rsct=image/png&skoid=6aaadede-4fb3-4698-a8f6-684d7786b067&sktid=a48cca56-e6da-484e-a814-9c849652bcb3&skt=2022-12-20T14%3A05%3A06Z&ske=2022-12-21T14%3A05%3A06Z&sks=b&skv=2021-08-06&sig=3GhYtK%2BxtryZApuJfM8yC7OQeyBMggDwiOfOScp4MBY%3D";
+
 export default async function (req, res) {
-  const completion = await openai.createCompletion({
-    model: "text-davinci-002",
+  const completion = await openai.createImage({
     prompt: generatePrompt(req.body.animal),
-    temperature: 0.6,
+    num_images: 3,
+    size: "512x512",
   });
-  res.status(200).json({ result: completion.data.choices[0].text });
+  console.log(completion.data);
+  res.status(200).json({ result: completion.data.data });
 }
 
 function generatePrompt(animal) {
   const capitalizedAnimal =
     animal[0].toUpperCase() + animal.slice(1).toLowerCase();
-  return `Suggest three names for an animal that is a superhero.
-
-Animal: Cat
-Names: Captain Sharpclaw, Agent Fluffball, The Incredible Feline
-Animal: Dog
-Names: Ruff the Protector, Wonder Canine, Sir Barks-a-Lot
-Animal: ${capitalizedAnimal}
-Names:`;
+  return `${capitalizedAnimal} RPG character`;
 }
--- pages/index.js
+++ pages/index.js
@@ -1,46 +1,94 @@
 import Head from "next/head";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import styles from "./index.module.css";
 
 export default function Home() {
-  const [animalInput, setAnimalInput] = useState("");
+  const [nameInput, setNameInput] = useState("");
   const [result, setResult] = useState();
+  const [pictureResult, setPictureResult] = useState();
+  const [nameInputPicture, setNameInputPicture] = useState("");
 
   async function onSubmit(event) {
     event.preventDefault();
+    // const response = await fetch("/api/generate", {
+    //   method: "POST",
+    //   headers: {
+    //     "Content-Type": "application/json",
+    //   },
+    //   body: JSON.stringify({ animal: nameInput }),
+    // });
+    // const data = await response.json();
+
+    const nameResponse = await fetch("/api/name", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ animal: nameInput  }),
+    });
+    const nameData = await nameResponse.json();
+
+    setResult(nameData.result);
+    // setPictureResult(data);
+    // console.log(pictureResult);
+    setNameInput("");
+    console.log(nameInputPicture);
+  }
+
+  async function onPictureGenerate() {
     const response = await fetch("/api/generate", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify({ animal: animalInput }),
+      body: JSON.stringify({ animal: `${result} ${nameInputPicture} RPG Character` }),
     });
     const data = await response.json();
-    setResult(data.result);
-    setAnimalInput("");
+    setPictureResult(data.result.map(picture => {
+      return <img key={picture.url} src={picture.url} alt= "character picture" />
+    }))
+    
+    // setPictureResult(data);
   }
+
+  // useEffect(() => {
+  //   onPictureGenerate();
+  // }, [result])
+  
 
   return (
     <div>
       <Head>
-        <title>OpenAI Quickstart</title>
-        <link rel="icon" href="/dog.png" />
+        <title>RPG Name Generator</title>
+        <link rel="icon" href="/character.png" />
       </Head>
 
       <main className={styles.main}>
-        <img src="/dog.png" className={styles.icon} />
-        <h3>Name my pet</h3>
+        <img src="/character.png" className={styles.icon} />
+        <h3>Your character's class</h3>
+
         <form onSubmit={onSubmit}>
           <input
             type="text"
             name="animal"
-            placeholder="Enter an animal"
-            value={animalInput}
-            onChange={(e) => setAnimalInput(e.target.value)}
+            placeholder="Enter a class e.g., wizard, ninja"
+            value={nameInput}
+            onChange={(e) => {
+                setNameInput(e.target.value)
+                setNameInputPicture(e.target.value)
+            }}
           />
           <input type="submit" value="Generate names" />
         </form>
         <div className={styles.result}>{result}</div>
+        
+        <div>
+            <button onClick={onPictureGenerate}>Generate Images</button>
+            <div>
+              {pictureResult}
+            </div>
+        </div>
+      
       </main>
     </div>
   );
